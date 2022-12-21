82175b09038ec9a6735d7629745a3891ea780d19
Francisco Navarro F <francisconavarroflores@gmail.com>
2022-12-21 10:49:11 -0300

    agregado link google mapss

--- src/components/Imagen.jsx
+++ src/components/Imagen.jsx
@@ -1,17 +1,25 @@
 import Card from 'react-bootstrap/Card';
+import Alert from 'react-bootstrap/Alert';
 import googlemaps from '../assets/img/googleimages.png';
+
+const styles = {
+  textoazul: {
+    color: "#4299E1",
+    fontWeight: "bold",
+  }
+}
 
 function Imagen1() {
   return (
     <Card className="bg-dark text-white">
       <Card.Img src={googlemaps} alt="Card image" />
       <Card.ImgOverlay>
-        <Card.Title>Card title</Card.Title>
-        <Card.Text>
-          This is a wider card with supporting text below as a natural lead-in
-          to additional content. This content is a little bit longer.
+      <Alert.Link href="https://www.google.com/maps/place/Hipódromo+Chile/@-33.4094399,-70.6641251,14z/data=!4m5!3m4!1s0x0:0x9ebd3e6263751d47!8m2!3d-33.4047827!4d-70.6664425" style={styles.textoazul}>Ver en Google Maps</Alert.Link>
+        <Card.Title > 
+        </Card.Title>
+        <Card.Text style={styles.textoazul}>
+        Av. Fermín Vivaceta 1372, Independencia, Región Metropolitana
         </Card.Text>
-        <Card.Text>Last updated 3 mins ago</Card.Text>
       </Card.ImgOverlay>
     </Card>
   );
